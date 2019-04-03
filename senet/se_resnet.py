6813f6f08cc642eaa9aaae816d51df57cce11f2b
Shangbang Long <shangbang.long@pku.edu.cn>
2019-04-03 16:27:00 +0800

    update

--- senet/se_resnet.py
+++ senet/se_resnet.py
@@ -8,7 +8,7 @@ from torchvision.models import ResNet
 from senet.se_layer import SqueezeExcitationLayer as SE
 
 __all__ = ['se_resnet18', 'se_resnet34', 'se_resnet50', 'se_resnet101',
-           'se_resnet152']
+           'se_resnet152', 'se_resnet20', 'se_resnet32', 'se_resnet56']
 
 model_urls = {
     'se_resnet18': None,
@@ -25,7 +25,7 @@ def conv3x3(in_planes, out_planes, stride=1):
 class SEBasicBlock(nn.Module):
     expansion = 1
 
-    def __init__(self, inplanes, planes, stride=1, downsample=None, reduction=16):
+    def __init__(self, inplanes, planes, stride=1, reduction=16):
         super(SEBasicBlock, self).__init__()
         self.conv1 = conv3x3(inplanes, planes, stride)
         self.bn1 = nn.BatchNorm2d(planes)
@@ -33,7 +33,11 @@ class SEBasicBlock(nn.Module):
         self.conv2 = conv3x3(planes, planes, 1)
         self.bn2 = nn.BatchNorm2d(planes)
         self.se = SE(planes, reduction)
-        self.downsample = downsample
+        if inplanes != planes:
+            self.downsample = nn.Sequential(nn.Conv2d(inplanes, planes, kernel_size=1, stride=stride, bias=False),
+                                            nn.BatchNorm2d(planes))
+        else:
+            self.downsample = lambda x: x
         self.stride = stride
 
     def forward(self, x):
@@ -152,3 +156,74 @@ def se_resnet152(num_classes=10):
     model.avgpool = nn.AdaptiveAvgPool2d(1)
     return model
 
+class CifarSENet(nn.Module):
+    """
+    This is specially designed for cifar10
+    """
+    def __init__(self, block, n_size, num_classes=10, reduction=16):
+        super(CifarSENet, self).__init__()
+        self.inplane = 16
+        self.conv1 = nn.Conv2d(3, self.inplane, kernel_size=3, stride=1, padding=1, bias=False)
+        self.bn1 = nn.BatchNorm2d(self.inplane)
+        self.relu = nn.ReLU(inplace=True)
+        self.layer1 = self._make_layer(block, 16, blocks=n_size, stride=1, reduction=reduction)
+        self.layer2 = self._make_layer(block, 32, blocks=n_size, stride=2, reduction=reduction)
+        self.layer3 = self._make_layer(block, 64, blocks=n_size, stride=2, reduction=reduction)
+        self.avgpool = nn.AdaptiveAvgPool2d(1)
+        self.fc = nn.Linear(64, num_classes)
+        self.initialize()
+
+    def initialize(self):
+        for m in self.modules():
+            if isinstance(m, nn.Conv2d):
+                nn.init.kaiming_normal_(m.weight)
+            elif isinstance(m, nn.BatchNorm2d):
+                nn.init.constant_(m.weight, 1)
+                nn.init.constant_(m.bias, 0)
+
+    def _make_layer(self, block, planes, blocks, stride, reduction):
+        strides = [stride] + [1] * (blocks - 1)
+        layers = []
+        for stride in strides:
+            layers.append(block(self.inplane, planes, stride, reduction))
+            self.inplane = planes
+
+        return nn.Sequential(*layers)
+
+    def forward(self, x):
+        x = self.conv1(x)
+        x = self.bn1(x)
+        x = self.relu(x)
+
+        x = self.layer1(x)
+        x = self.layer2(x)
+        x = self.layer3(x)
+
+        x = self.avgpool(x)
+        x = x.view(x.size(0), -1)
+        x = self.fc(x)
+
+        return x
+
+def se_resnet20(**kwargs):
+    """Constructs a ResNet-18 model.
+
+    """
+    model = CifarSENet(SEBasicBlock, 3, **kwargs)
+    return model
+
+
+def se_resnet32(**kwargs):
+    """Constructs a ResNet-34 model.
+
+    """
+    model = CifarSENet(SEBasicBlock, 5, **kwargs)
+    return model
+
+
+def se_resnet56(**kwargs):
+    """Constructs a ResNet-34 model.
+
+    """
+    model = CifarSENet(SEBasicBlock, 9, **kwargs)
+    return model
